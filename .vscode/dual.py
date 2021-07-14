9599d36b428741e3805780b6d3534ec0656f130a
Sarith Vithanage <nvskalhara.99@gmail.com>
2021-07-15 02:43:23 +0530

    Second Commoit

--- .vscode/dual.py
+++ .vscode/dual.py
@@ -21,5 +21,25 @@ def dual_once(p):
 
 
 p = np.array([4/6, 5/6])
+s = np.zeros(2)
+total_turns =  0
+dual_num = 10000
+print("Number of Duals = ", dual_num)
 
+for i in range(0, dual_num):
+    (survivor, turn_num) = dual_once(p)
+    s[survivor] = s[survivor] + 1
+    total_turns = total_turns + turn_num
+print(s)
+Probability_of_Winning = s/dual_num
+
+print("---Simulation Results---")
+print("Probability of A Winning = ", Probability_of_Winning[0])
+print("Probability of B Winning = ", Probability_of_Winning[1])
+turn_average = total_turns/dual_num
+print("Turn Average = ", turn_average)
+
+print("---Theoritical Values---")
+print("Theoritical Probability of A Winning = ", 12/17)
+print("Theoritical Probability of B Winning = ", 5/17)
     
